a8034ec0286f114fbf31ca22bd6fb9174ad03530
Renan Rafael Bertoldo <renan10r_@hotmail.com>
2023-05-15 22:30:33 +0100

    new get two urls

--- search.py
+++ search.py
@@ -1,25 +1,48 @@
 import requests
 import json
+import datetime
 
-url = 'https://spaceweather.gfz-potsdam.de/fileadmin/ruggero/Kp_forecast/forecast_figures/KP_FORECAST_CURRENT.dat'
-response = requests.get(url)
+# Function to fetch data from the first site
+def fetch_site1_data():
+    url = 'https://spaceweather.gfz-potsdam.de/fileadmin/ruggero/Kp_forecast/forecast_figures/KP_FORECAST_CURRENT.dat'
+    response = requests.get(url)
+    data = response.text.split('\n')[:-1]
+    result = []
+    for line in data:
+        year, day, hour, kp = line.split()
+        date = get_date(int(year), int(day), int(hour))
+        result.append({'datetime': f'{date}', 'kp': float(kp)})
+    return result
 
-data = response.text
+# Function to fetch data from the second site
+def fetch_site2_data():
+    url = 'https://kp.gfz-potsdam.de/app/json/?start=2023-05-15T00%3A00%3A00Z&end=2023-05-15T23%3A59%3A59Z&index=Kp#kpdatadownload-143'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    datetime = data['datetime']
+    kp = data['Kp']
+    result = []
+    for dt, kp_value in zip(datetime, kp):
+        result.append({'datetime': dt, 'kp': kp_value})
+    return result
 
-# Transformar a resposta em uma lista de dicionários
-forecast_list = []
-lines = data.split('\n')
-for line in lines:
-    if line:
-        values = line.split()
-        forecast = {
-            'year': int(values[0]),
-            'doy': int(values[1]),
-            'hour': int(values[2]),
-            'kp': float(values[3])
-        }
-        forecast_list.append(forecast)
+# Function to merge the data and save it to a local JSON file
+def merge_and_save_data():
+    site1_data = fetch_site1_data()
+    site2_data = fetch_site2_data()
+    indexInsert = 0
+    for item in site2_data:
+        if item['datetime'] not in [i['datetime'] for i in site1_data]:
+            site1_data.insert(indexInsert, item)
+            indexInsert += 1
 
-# Salvar o resultado em formato JSON no diretório 'data' do repositório
-with open('new_kp.json', 'w') as file:
-    json.dump(forecast_list, file)
+    with open('new_kp.json', 'w') as file:
+        json.dump(site1_data, file, indent=4)
+    print('Merged data successfully saved!')
+
+def get_date(year, day_of_year, hour):
+    date = datetime.datetime(year, 1, 1) + datetime.timedelta(day_of_year - 1)
+    return f'{date.strftime("%Y-%m-%d")}T{str(hour).zfill(2)}:00:00Z'
+
+# Call the function to merge and save the data
+merge_and_save_data()
