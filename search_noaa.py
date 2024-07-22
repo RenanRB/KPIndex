9a2e5d7211d6e2ecc729854d2a07d1114036cab6
Renan Rafael Bertoldo <renan10r_@hotmail.com>
2024-07-22 20:52:28 -0300

    Add files via upload

--- search_noaa.py
+++ search_noaa.py
@@ -1,45 +1,40 @@
-import csv
-import json
 import requests
+import json
 from datetime import datetime, timedelta, time
 
-def fetch_site1_data():
-    fixed_hours = [3, 6, 9, 12, 15, 18, 21, 23]
-    url = 'https://spaceweather.gfz-potsdam.de/fileadmin/Kp-Forecast/CSV/kp_product_file_FORECAST_PAGER_SWIFT_LAST.csv'
+url_hour = 'https://services.swpc.noaa.gov/text/3-day-geomag-forecast.txt'
+url_daily = 'https://services.swpc.noaa.gov/text/27-day-outlook.txt2'
+
+def fetch_and_process_hour_data(url):
     response = requests.get(url, verify=False)
-    lines = response.text.split('\n')
+    if (response.status_code != 200):
+        return []
+    data = response.text.splitlines()
 
-    forecast_list = []
-    for line in csv.reader(lines[1:]):
-        if len(line) > 1:
-            date_time_str = line[0]
-            kp = float(line[5])
-            date_time_obj = datetime.strptime(date_time_str, '%d-%m-%Y %H:%M')
-            closest_hour = min(fixed_hours, key=lambda x: abs(x - date_time_obj.hour))
-            if closest_hour == 23:
-                closest_hour = 0
-                date_time_obj += timedelta(days=1)
-            date_time_obj = date_time_obj.replace(hour=closest_hour)
-            forecast_list.append({
-                "datetime": date_time_obj.strftime('%Y-%m-%dT%H:%M:%SZ'),
-                "kp": kp
-            })
-    return forecast_list
+    kp_data = [[],[],[]]
+    start_date = None
 
-def fetch_site2_data():
-    today = datetime.now().strftime('%Y-%m-%d')
-    url = f'https://kp.gfz-potsdam.de/app/json/?start={today}T00%3A00%3A00Z&end={today}T23%3A59%3A59Z&index=Kp#kpdatadownload-143'
-    response = requests.get(url, verify=False)
-    data = json.loads(response.text)
-    date_time_obj = data['datetime']
-    kp = data['Kp']
-    result = []
-    for dt, kp_value in zip(date_time_obj, kp):
-        result.append({'datetime': dt, 'kp': kp_value})
-    return result
+    for i, line in enumerate(data):
+        splited_lide = line.split()
+        if line.startswith(':Issued:'):
+            date_time_str = line.split(":Issued: ")[1]
+            date_time_str = date_time_str.replace(" UTC", "")
+            date_time = datetime.strptime(date_time_str, '%Y %b %d %H%M')
+            next_day = date_time + timedelta(days=1)
+            start_date = next_day.replace(hour=0, minute=0, second=0, microsecond=0)
+        if start_date and len(splited_lide) == 4 and 'UT' in splited_lide[0]:
+            for j, kp in enumerate(splited_lide[1:]):
+                kp_value = kp
+                datetime_str = (start_date + timedelta(days=j)).strftime('%Y-%m-%dT%H:%M:%SZ')
+                kp_data[j].append({
+                    "datetime": datetime_str,
+                    "kp": kp_value
+                })
+            start_date = start_date + timedelta(hours=3)
+    
+    return kp_data[0] + kp_data[1] + kp_data[2]
 
-def fetch_site3_data():
-    url = 'https://services.swpc.noaa.gov/text/27-day-outlook.txt2'
+def fetch_and_process_daily_data(url):
     response = requests.get(url, verify=False)
     data = response.text.splitlines()
     start_date = None
@@ -101,22 +96,11 @@ def merge_infos(kp_hour_data, kp_daily_data):
     return join_data
 
 def merge_and_save_data():
-    site1_data = fetch_site1_data()
-    site2_data = fetch_site2_data()
-    site3_data = fetch_site3_data()
-    indexInsert = 0
-    for item in site2_data:
-        if item['datetime'] not in [i['datetime'] for i in site1_data]:
-            site1_data.insert(indexInsert, item)
-            indexInsert += 1
-
-    result = merge_infos(site1_data, site3_data)
+    kp_hour_data = fetch_and_process_hour_data(url_hour)
+    kp_daily_data = fetch_and_process_daily_data(url_daily)
+    result = merge_infos(kp_hour_data, kp_daily_data)
 
     with open('new_kp.json', 'w') as file:
         json.dump(result, file, indent=4)
-
-def get_date(year, day_of_year, hour):
-    date = datetime.datetime(year, 1, 1) + datetime.timedelta(day_of_year - 1)
-    return f'{date.strftime("%Y-%m-%d")}T{str(hour).zfill(2)}:00:00Z'
 
 merge_and_save_data()
